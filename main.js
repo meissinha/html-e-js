4f45298f1431e665689c4c5f0a826c76f92a19cf
mahguerra08 <129458082+mahguerra08@users.noreply.github.com>
2023-11-16 20:24:44 -0300

    16

---
+++ main.js
@@ -0,0 +1,34 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        
+        console.log(evento.cod==)
+
+        if(evento.cod = 'Space'){
+
+            tecla.classList.add('ativa');
+        }
+    
+        
+    
+
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
