990277c18bd2313ae16cb36115f79265f473ffab
Mei Tanamati <108132708+meissinha@users.noreply.github.com>
2023-12-07 22:08:59 -0300

    Update main.js
    0000

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    
+    if(elemento != null && elemento.localName === 'audio') {
+      elemento.play();
+    }
+    
+  else{
+    console.log('elemento não encontrado ou seletor inválido');
+    
+   }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,19 +26,20 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        
-        if (evento.code === 'Space' || evento.code === 'Enter'){
-            tecla.classList.add('ativa');    
+    tecla.onkeydown = function(evento){
+
+       
+        if (evento.code === 'Space' ||evento.code === 'Space') {
+            tecla.classList.add('ativa');
         }
 
-        
-        
-    }
 
-    tecla.onkeyup = function () {
+    }
+    
+    tecla.onkeyup = function() {
         tecla.classList.remove('ativa');
     }
+
 
 }
 
